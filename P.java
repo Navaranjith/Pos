e8bbce2223755d2cd90724cfbad89c548723d61f
Navaranjith <40588106+Navaranjith@users.noreply.github.com>
2018-07-01 21:59:32 +0530

    Create P.java

---
+++ P.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class prim
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a=s.nextInt();
+        int k=0,x=0;
+        int p=0;
+        for(int i=n;i<a;i++)
+        {
+            int c=0;
+            for(k=i;k>=1;k--)
+            {
+               if(i%k==0)
+               {
+                  c+=1;
+               }
+            }
+            if(c==2)
+            {
+                p++;
+            }
+        }
+        System.out.print(p);
+    }
+}
+
