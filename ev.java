00b0c219e2a68f64e3e28c7836d14978855ef256
Navaranjith <40588106+Navaranjith@users.noreply.github.com>
2018-07-01 15:02:32 +0530

    Create ev.java

---
+++ ev.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class ev
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a=s.nextInt();
+        if(a<=100000)
+        {
+            for(int i=n+1;i<a;i++)
+            {
+                if(i%2==0)
+                {
+                    System.out.print(i+" ");
+                }
+            }
+        }
+    }
+}
