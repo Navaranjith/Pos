186bb4eb6d5e94e6fca8da3148e6a299461e2867
Navaranjith <40588106+Navaranjith@users.noreply.github.com>
2018-07-01 08:17:48 +0530

    Create He.java

---
+++ He.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class He
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        String a="Hello";
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(a);
+        }
+    }
+}
