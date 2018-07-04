7ab04b464887f601197a3d8430090e4c1eb5abf0
Navaranjith <40588106+Navaranjith@users.noreply.github.com>
2018-07-04 21:34:50 +0530

    Update Cam.java

--- Cam.java
+++ Cam.java
@@ -4,17 +4,16 @@ class Cam
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
-        String a=s.nextLine();
+        String a=s.nextLine().toLowerCase();
         String[] n=a.split(" ");
         String b="";
-        String c="",x="",y="";
+        String c="";
         for(int i=0;i<n.length;i++)
         {
             b=n[i].substring(0,1);
-            x=b.toUpperCase();
+            b=b.toUpperCase();
             c=n[i].substring(1);
-            y=x+c;
-            System.out.print(y+" ");
+            System.out.print(b+c+" ");
         }
     }
 }
